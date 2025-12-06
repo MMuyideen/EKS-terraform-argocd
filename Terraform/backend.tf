4480bc3f881d4be7ab9694931b3c8249ae22249d
Morex <dejimorenigbade@gmail.com>
2025-12-06 19:40:38 +0000

    clean up terraform

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "week4-todo-tf-s3"
-#     key    = "backend/ToDo-App.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "week4-todo-tf-s3"
+    key    = "backend/ToDo-App.tfstate"
+    region = "us-east-1"
+  }
+}
 
--- Terraform/modules/NodeGroup/main.tf
+++ Terraform/modules/NodeGroup/main.tf
@@ -1,14 +1,10 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_node_group
 
 resource "aws_eks_node_group" "node_group" {
-  # Name of the EKS Cluster
-  cluster_name = var.EKS_CLUSTER_NAME
 
-  # Name of the EKS Node Group.
-  node_group_name = "${var.EKS_CLUSTER_NAME}-node_group"
-
-  # Amazon Resource Name (ARN) of the IAM Role that provides permissions for the EKS Node Group.
-  node_role_arn = var.NODE_GROUP_ARN
+  cluster_name    = var.EKS_CLUSTER_NAME                 # Name of the EKS Cluster
+  node_group_name = "${var.EKS_CLUSTER_NAME}-node_group" # Name of the EKS Node Group.
+  node_role_arn   = var.NODE_GROUP_ARN                   # ARN of the IAM Role that provides permissions for the EKS Node Group.
 
   # Identifiers of EC2 Subnets to associate with the EKS Node Group. 
   # These subnets must have the following resource tag: kubernetes.io/cluster/EKS_CLUSTER_NAME 
@@ -20,38 +16,23 @@ resource "aws_eks_node_group" "node_group" {
 
   # Configuration block
   scaling_config {
-    # Required number of worker nodes
-    desired_size = 2
-
-    # Maximum number of worker nodes
-    max_size = 2
-
-    # Minimum number of worker nodes
-    min_size = 2
+    desired_size = 2 # Required number of worker nodes
+    max_size     = 2 # Maximum number of worker nodes
+    min_size     = 2 # Minimum number of worker nodes
   }
 
-  # Type of Amazon Machine Image (AMI) associated with the EKS Node Group
+  ami_type      = "AL2_x86_64" # AMI type associated with the EKS Node Group
+  capacity_type = "ON_DEMAND"  # capacity type associated with the EKS Node Group
+  disk_size     = 20           # Disk size in GB for worker nodes
 
-  ami_type = "AL2_x86_64"
-
-  # Type of capacity associated with the EKS Node Group
-
-  capacity_type = "ON_DEMAND"
-
-  # Disk size in GB for worker nodes
-  disk_size = 20
-
-  # Force version update if existing pods are unable to be drained due to a pod disruption budget issue
-  force_update_version = false
-
-  # Instance type associated with the EKS Node Group
-  instance_types = ["t3.small"]
+  
+  force_update_version = false # Force version update if existing pods are unable to be drained due to a pod disruption budget issue
+  instance_types = ["t3.small"] # Instance type associated with the EKS Node Group
 
   labels = {
     role = "${var.EKS_CLUSTER_NAME}-Node-group-role",
     name = "${var.EKS_CLUSTER_NAME}-node_group"
   }
 
-  # Kubernetes version
-  version = "1.27"
+  version = "1.31" # Kubernetes version for the EKS Node Group
 }
