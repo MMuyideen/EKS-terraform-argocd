9884288a06714cd8eb89d8382965d98c46279aea
Morex <dejimorenigbade@gmail.com>
2025-12-06 17:45:41 +0000

    cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -8,3 +8,8 @@ terraform destroy -auto-approve
 # empty bucket
 aws s3 rb s3://$S3_BUCKET_NAME \
     --force
+
+# delete s3 bucket
+aws s3api delete-bucket \
+    --bucket $S3_BUCKET_NAME \
+    --region us-east-1
